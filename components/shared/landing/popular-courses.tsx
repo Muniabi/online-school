d5bc6ce4a8f5e7257e2d221c15566eb8a1624602
Muniabi <infotkahn@yandex.ru>
2024-11-06 01:52:18 +0300

    добавил слайдер

--- components/shared/landing/popular-courses.tsx
+++ components/shared/landing/popular-courses.tsx
@@ -1,5 +1,55 @@
 import React from "react";
 import { Container } from "../container";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Badge, Button } from "@/components/ui";
+import { ArrowUpRight } from "lucide-react";
+
+// Массив данных для курсов
+const coursesData = [
+    {
+        id: 1,
+        title: "Основы HTML",
+        type: "HTML",
+        image: "/popular-courses/course1.png",
+        rating: 4.5,
+        reviewsCount: 150,
+        price: "5000₽",
+    },
+    {
+        id: 2,
+        title: "Design с нуля",
+        type: "Design",
+        image: "/popular-courses/course2.png",
+        rating: 4.8,
+        reviewsCount: 250,
+        price: "7500₽",
+    },
+    {
+        id: 3,
+        title: "Основы Business",
+        type: "Business",
+        image: "/popular-courses/course3.png",
+        rating: 4.7,
+        reviewsCount: 300,
+        price: "4500₽",
+    },
+    {
+        id: 4,
+        title: "Social Media",
+        type: "Social Media",
+        image: "/popular-courses/course4.png",
+        rating: 4.9,
+        reviewsCount: 500,
+        price: "10000₽",
+    },
+];
 
 interface PopularCoursesProps {
     className?: string;
@@ -7,11 +57,68 @@ interface PopularCoursesProps {
 
 const PopularCourses = ({ className }: PopularCoursesProps) => {
     return (
-        <div className="bg-[#242424]">
-            <Container>
+        <div className="bg-[#FFF7EE] dark:bg-[#242424] overflow-hidden">
+            <Container className="flex items-center justify-evenly py-24">
                 {/* Левая часть */}
-                поопоплпорпро
+                <div className="text-5xl font-[600] leading-[72px]">
+                    Самые <br />{" "}
+                    <span className="text-[--purple]">
+                        Популярные <br /> Курсы
+                    </span>
+                </div>
+
                 {/* Слайдер */}
+                <Carousel className="w-full max-w-xs">
+                    <CarouselContent className="">
+                        {coursesData.map((course) => (
+                            <CarouselItem key={course.id}>
+                                <div>
+                                    <Card className="bg-white dark:bg-white">
+                                        <CardContent className="p-4">
+                                            {/* Картинка Курса */}
+                                            <div className="relative p-0 m-0">
+                                                <img
+                                                    src={course.image}
+                                                    alt={course.title}
+                                                    className="w-full object-cover rounded-lg aspect-[12/9]"
+                                                />
+                                                <Badge className="absolute top-2 left-2 text-sm border-none px-2 py-1 text-black dark:text-white rounded-md bg-white hover:bg-white dark:bg-[#1F221F]">
+                                                    {course.type}
+                                                </Badge>
+                                            </div>
+                                            <h3 className="pt-2 text-xl text-[#363A3D] dark:text-[#] font-medium">
+                                                {course.title}
+                                            </h3>
+                                            <div className="flex mt-2 space-x-2">
+                                                <span className="text-[--yellow]">
+                                                    ⭐⭐⭐⭐⭐
+                                                </span>
+                                                <span className="text-gray-400">
+                                                    ({course.reviewsCount})
+                                                </span>
+                                            </div>
+                                        </CardContent>
+                                        <CardFooter className=" border-t border-[#E7E9EB]">
+                                            {/* Информация о курсе */}
+                                            <div className="flex items-center justify-between w-full mt-4">
+                                                <p className="text-lg text-black font-semibold">
+                                                    {course.price}
+                                                </p>
+                                                <Button variant="secondary">
+                                                    <ArrowUpRight className="h-4 w-4" />
+                                                </Button>
+                                            </div>
+                                        </CardFooter>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <div className="py-4 flex items-center justify-center gap-4">
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </div>
+                </Carousel>
             </Container>
         </div>
     );
