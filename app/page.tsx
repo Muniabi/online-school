163025c6db2294788a766dfa8198f5aa8c899f75
Muniabi <infotkahn@yandex.ru>
2024-11-18 23:25:37 +0300

    add lamp

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { MainInfoBlock } from "@/components/shared/landing/main-info";
 import { ContainerScroll } from "@/components/shared/landing/container-scroll-animation";
 import PopularCourses from "@/components/shared/landing/popular-courses";
@@ -5,6 +6,8 @@ import PopularCategories from "@/components/shared/landing/popular-categories";
 import BestAuthors from "@/components/shared/landing/best-authors";
 import Integrations from "@/components/shared/landing/Integrations";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { LampContainer } from "@/components/shared/landing/lamp";
 
 export default function Home() {
     return (
@@ -14,9 +17,9 @@ export default function Home() {
                     titleComponent={
                         <>
                             <h1 className="text-4xl font-semibold text-black dark:text-white">
-                                Unleash the power of <br />
+                                Открой возможности <br />
                                 <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                                    Scroll Animations
+                                    IT-образования
                                 </span>
                             </h1>
                         </>
@@ -34,7 +37,20 @@ export default function Home() {
             </div>
             <MainInfoBlock />
             <PopularCourses />
-
+            <LampContainer>
+                <motion.h1
+                    initial={{ opacity: 0.5, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{
+                        delay: 0.3,
+                        duration: 0.8,
+                        ease: "easeInOut",
+                    }}
+                    className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+                >
+                    Популярные курсы <br /> от лучших авторов
+                </motion.h1>
+            </LampContainer>
             <Integrations />
             <PopularCategories />
             <BestAuthors />
